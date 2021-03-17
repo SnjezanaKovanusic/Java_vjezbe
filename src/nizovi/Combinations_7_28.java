d3ddc0f09b7f4a743caa9b57a224480685d77a13
SnjezanaKovanusic <snjezanakovanusic@gmail.com>
2021-03-17 21:57:19 +0100

    dodali konbinacije u nizu

--- src/nizovi/Combinations_7_28.java
+++ src/nizovi/Combinations_7_28.java
@@ -4,7 +4,7 @@ public class Combinations_7_28 {
     public static void main(String[] args) {
         int [] niz = {1,2,3,4};
         for (int i =0; i<niz.length; i++){
-            for (int j = 0; i <niz.length; j++){
+            for (int j = 0; j <niz.length; j++){
                 System.out.println(niz[i]+" "+ niz[j]);
                 }
             }
