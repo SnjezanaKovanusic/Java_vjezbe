8b0e073a8fd9e14b16cee04612987e2902569af7
SnjezanaKovanusic <snjezanakovanusic@gmail.com>
2021-03-25 13:52:38 +0100

    sort characters in string

---
+++ src/nizovi/SortCharactersInString.java
@@ -0,0 +1,38 @@
+package nizovi;
+
+import java.util.Arrays;
+
+public class SortCharactersInString {
+    /*(Sort characters in a string) Write a method that returns a sorted string using the
+following header:
+public static String sort(String s)
+For example, sort("acb") returns abc.
+Write a test program that prompts the user to enter a string and displays the sorted
+string.*/
+    public static void main(String[] args) {
+        String rijec = "voooojislav";
+        String sort = sortPjeske(rijec);
+
+        System.out.println(sort);
+
+    }
+
+    private static String sort(String rijec) {
+        char sortirana[] = rijec.toCharArray();
+        Arrays.sort(sortirana);
+        return String.valueOf(sortirana);
+    }
+
+    private static String sortPjeske(String rijec) {
+        char sotiranaRijec[] = new char[rijec.length()];
+        for (int i = 0; i < sotiranaRijec.length; i++) {
+            sotiranaRijec[i] = rijec.charAt(i);
+        }
+        Arrays.sort(sotiranaRijec);
+        String sortiraniString = "";
+        for (int i = 0; i < sotiranaRijec.length; i++) {
+            sortiraniString += sotiranaRijec[i];
+        }
+        return sortiraniString;
+    }
+}
